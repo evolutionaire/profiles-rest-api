71de3e3b89b2f96192029e5264b198a6825ee279
Michael Tottrup <michael.tottrup@gmail.com>
2020-01-12 21:08:44 -0800

    Trying to fix superuser model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -20,9 +20,9 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self, email, name, password):
+    def create_superuser(self, email=None, name=None, password=None):
         """Create a new superuser with given details"""
-        user = self.create_user(email, name, password)
+        user = self.create_user(email=email, name=name, password=password)
 
         user.is_superuser = True
         user.is_staff = True
