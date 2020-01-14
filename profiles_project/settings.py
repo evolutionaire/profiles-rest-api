666c8003d67a115e135712c104006a7e5a4e632d
Michael Tottrup <michael.tottrup@gmail.com>
2020-01-13 17:59:53 -0800

    added hello api view

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')0^t%j@1vhh4@1sp71^2=p!9%n5tys$-8%q*a(u^7@=4e*8q1x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', ]
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1',]
 
 
 # Application definition
